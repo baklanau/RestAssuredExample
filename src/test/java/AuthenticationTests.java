6191ba728bee300e0d3cc9d7a81246b55755b7b4
Dmitry Baklanov <dmitry.baklanov@mainsoft.org>
2019-06-30 18:41:24 +0300

    Authentication

---
+++ src/test/java/AuthenticationTests.java
@@ -0,0 +1,71 @@
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class AuthenticationTests {
+
+    @BeforeClass
+    public static void setup() {
+        RestAssured.proxy("localhost", 8889);
+    }
+
+    //42
+    @Test
+    public void basicPreemtiveAuthTest() {
+
+        given().
+                auth().preemptive().basic("username", "password").
+        when().
+                get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void basicChallengedAuthTest() {
+
+        given().
+                auth().basic("username", "password").
+        when().
+                get("http://localhost:8080/someEndpoint");
+    }
+
+    //43
+    @Test
+    public void oauth1Test() {
+
+        given().
+                auth().oauth("consumerKey", "consumerSecret", "accessToken", "secretToken").
+        when().
+                get("http://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void oauth2Test() {
+
+        given().
+                auth().oauth2("accessToken").
+        when().
+                get("http://localhost:8080/someEndpoint");
+    }
+
+    //44
+    @Test
+    public void relaxedHTTPSTest() {
+
+        given().
+                relaxedHTTPSValidation().
+        when().
+                get("https://localhost:8080/someEndpoint");
+    }
+
+    @Test
+    public void keyStoreTest() {
+
+        given().
+                keyStore("/pathToJKS", "password").
+        when().
+                get("https://localhost:8080/someEndpoint");
+    }
+
+}
